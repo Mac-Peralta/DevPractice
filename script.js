6f9b455d22f86c8d90ce41cc05253803cd113242
Mac-Peralta <ykcamperalta323@gmail.com>
2023-09-18 21:32:33 +0800

    added more functions

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
 const x = "test";
+const buttonOne = document.querySelector('#b1');
+const reveal = document.querySelector('#yaki');
+var buttonTwo = document.querySelector('#b2');
 function ifYes(){
-    document.getElementById("h").innerHTML = "Hello";
+    let ye = document.getElementById("h");
+    ye.innerHTML = "Hello";
+    ye.style.color = 'red';
+}
+function revealingArea() {
+    let x = document.getElementById('sova');
+    x.style.color = 'red';
+    if(reveal.classList.contains('yaki')){
+        reveal.classList.remove('yaki');
+    }
+    else{
+        reveal.classList.add('yaki');
+    }
 }
  function ifNo(){
     document.getElementById("h").innerHTML = "World";
  }
+
+ buttonOne.addEventListener("click", ifYes);
+ buttonTwo.addEventListener("click", ifNo);
+ reveal.addEventListener("click", revealingArea);
